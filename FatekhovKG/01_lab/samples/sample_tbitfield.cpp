fa0fa2f7ab14598c7a4dab5867a3359e07e22db8
Kamil Fatekhov <fatehovk@gmail.com>
2023-11-08 09:01:43 +0300

    final

--- FatekhovKG/01_lab/samples/sample_tbitfield.cpp
+++ FatekhovKG/01_lab/samples/sample_tbitfield.cpp
@@ -3,31 +3,33 @@
 
 int main()
 {
-    const int size1 = 4, size2 = 7;
-    TBitField set1(size1), set2(size2);
-    int a = (set1 != set2);
-    int b = (set1 == set2);
-    cout << "bf1 = " << set1 << endl << "b2 = " << set2 << endl;
+    cout << "------TBITFIELD_TEST------"<<endl;
+    const int size1 = 3, size2 = 5;
+    TBitField bf1(size1), bf2(size2);
+    cout << "Equal test:"<<endl;
+    int a = (bf1 != bf2);
+    int b = (bf1 == bf2);
+    cout << "bf1 = " << bf1 << endl << "bf2 = " << bf2 << endl;
     std::cout << "bf1 != bf2 -> " << a << endl << "bf1 == bf 2 -> " << b << std::endl;
-    TBitField c(1);
-    TBitField d(set1);
-    cout << "enter bf len4" << endl;
-    cin >> d;
-    cout << "bf3 = " << d << endl;
-    int z = d.GetLength();
-    cout << "bf3.lenght = " << z << endl;
-    d.ClrBit(1);
-    cout << "bf3.clrbit(1) = " << d << endl;
-    c = set2;
-    cout << "bf4 = bf2 -> " << c << endl;
-    c.SetBit(0);
-    cout << "bf4.setbit(0) -> " << c << endl;
-    set1 = c | d;
-    cout << "bf1 = bf3 | bf4 -> " << set1 << endl;
-    set2 = c & d;
-    cout << "bf2 = bf3 & bf4 -> " << set2 << endl;
-    c = ~d;
-    cout << "bf3 = ~bf4 -> " << c << endl;
+    TBitField bf3(bf1);
+    cout << "enter bitfield length 3" << endl;
+    cin >> bf3;
+    int c = bf3.GetLength();
+    cout << "Length of bf3 = " << c << endl;
+    bf3.SetBit(2);
+    cout << "SetBit(2) for bf3: " << bf3<<endl;
+    bf3.ClrBit(2);
+    cout << "ClrBit(2) for bf3: " << bf3<<endl;
+    const int g = bf3.GetBit(2);
+    cout << "GetBit(2) for bf3: " << g << endl;
+    bf2 = bf3;
+    cout << "bf2 = bf3 -> " << bf2 << endl;
+    bf2 = bf3 | bf1;
+    cout << "bf2 = bf3 | bf1 -> " << bf2 << endl;
+    bf2 = bf3 & bf1;
+    cout << "bf2 = bf3 & bf1 -> " << bf2 << endl;
+    bf2 = ~bf1;
+    cout << "bf2 = ~bf1 -> " << bf2 << endl;
     system("PAUSE");
     return 0;
 }
